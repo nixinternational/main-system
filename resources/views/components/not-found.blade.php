aa8304c2f0198afd13866cc0149df69797fb838d
Heryck Mota <heryckmota@gmail.com>
2025-10-20 23:36:31 -0300

    fix: simplify not found message in the not-found component

--- resources/views/components/not-found.blade.php
+++ resources/views/components/not-found.blade.php
@@ -1,4 +1,3 @@
 <div class="mt-3 d-flex justify-content-center flex-column">
-    <h3 class="text-center">Ops! Não foi encontrado nenhum registro com o filtro informado.</h3>
-    <img style="height: 50vh; margin:0 auto;" src="{{ asset('images/nodata.png') }}" alt="">
+    <h3 class="text-center">Ops! Não foi encontrado nenhum registro.</h3>
 </div>
