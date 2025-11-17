6bd2aa3dbfb28fee817bd2f37c41355dd4186311
Heryck Mota <heryckmota@gmail.com>
2025-11-17 12:45:48 -0300

    as

---
+++ database/migrations/2025_11_17_130000_add_active_column_to_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasColumn('users', 'active')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->boolean('active')
+                    ->default(true)
+                    ->after('remember_token');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('users', 'active')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('active');
+            });
+        }
+    }
+};
+
